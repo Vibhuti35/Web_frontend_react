fd53dbecd18361639c06019be4dccdd2ee54a575
Vibs <Vibs@TBDC3.WPA.Dal.Ca>
2018-06-30 10:52:54 -0300

    Altering CSS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './CSS/App.css';
+import './App.css';
 import Signin from './Login.jsx'; 
 import Signup from './Register.jsx'; 
 import HomePage from './Home.jsx'; 
